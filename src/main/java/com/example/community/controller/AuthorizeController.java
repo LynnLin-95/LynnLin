1bda30a025065b376677677a1b7ce37ff2614e7e
18782232934 <1448602924@qq.com>
2019-10-18 16:24:09 +0800

    在cookie中拿到token，获取用户信息

--- src/main/java/com/example/community/controller/AuthorizeController.java
+++ src/main/java/com/example/community/controller/AuthorizeController.java
@@ -12,7 +12,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.annotation.Resource;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 
 @Controller
@@ -30,13 +32,14 @@ public class AuthorizeController {
     @Value("${github.redirect.uri}")
     private String redirectUri;
 
-    @Autowired
+    @Resource
     private UserMapper userMapper;
 
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code") String code,
                            @RequestParam(name="state") String state,
-                           HttpServletRequest request) {
+                           HttpServletRequest request,
+                           HttpServletResponse response) {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setClient_secret(clientSecret);
@@ -51,12 +54,16 @@ public class AuthorizeController {
         if (githubUser != null){
             //登录成功，写cookie 和 session
             User user = new User();
-            user.setToken(UUID.randomUUID().toString());//UUID
+            String token = UUID.randomUUID().toString();//根据token来验证session，进行持久化
+            user.setToken(token);//UUID
             user.setName(githubUser.getName());
             user.setAccountId(String.valueOf(githubUser.getId()));//强转为String
             user.setGmtCreate(System.currentTimeMillis());//当前时间，毫秒
             user.setGmtModified(user.getGmtModified());
-            userMapper.insert(user);
+            userMapper.insert(user);//存到h2数据库
+
+            response.addCookie(new Cookie("token",token));
+
             request.getSession().setAttribute("user",githubUser);
             return "redirect:/";
         }
--- src/main/java/com/example/community/controller/IndexController.java
+++ src/main/java/com/example/community/controller/IndexController.java
@@ -1,14 +1,36 @@
 package com.example.community.controller;
 
+import com.example.community.mapper.UserMapper;
+import com.example.community.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.annotation.Resource;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class IndexController {
+
+    @Resource
+    private UserMapper userMapper;
+
     @GetMapping("/")
-    public String index(){
+    public String index(HttpServletRequest request){
+        Cookie[] cookies = request.getCookies();//返回一个cookie数组
+        for (Cookie cookie:cookies){
+            if (cookie.getName().equals("token")){
+                String token = cookie.getValue();//拿到token
+                User user = userMapper.findByToken(token);//根据token返回用户
+                if (user != null){
+                    request.getSession().setAttribute("user",user);//传回前端
+                }
+                break;
+            }
+        }
         return "index";
     }
 }
